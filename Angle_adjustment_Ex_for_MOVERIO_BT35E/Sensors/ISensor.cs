eab43c10f2caa93b7b76f8aeeb2b89defc51c145
Fukky <r.fukky21@gmail.com>
2020-01-29 10:53:42 +0900

    Add images

--- Angle_adjustment_Ex_for_MOVERIO_BT35E/Sensors/ISensor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Angle_adjustment_Ex_for_MOVERIO_BT35E.Sensors
-{
-    internal interface ISensor
-    {
-        void Active();
-
-        void Inactive();
-    }
-}
--- Angle_adjustment_Ex_for_MOVERIO_BT35E/Sensors/SensorData.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Angle_adjustment_Ex_for_MOVERIO_BT35E.Sensors
-{
-    public class SensorData
-    {
-        public double Value { get; set; }
-    }
-}
